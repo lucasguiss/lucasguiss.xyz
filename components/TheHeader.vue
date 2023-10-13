0f55d44e233bfd7b431be382be09f6add3b9fc68
Lucas Guiss <lucasguissgusmao@gmail.com>
2023-10-13 19:51:02 -0300

    fix: add correct html tag to menu

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,9 +1,9 @@
 <template>
 <header class="w-full top-0 p-5 flex flex-row-reverse">
     <nav class="flex flex-row gap-x-10">
-      <a class="text-lg text-lime cursor-pointer hover:text-mint font-medium" @click="store.setContent('home')">home</a>
-      <a class="text-lg text-lime cursor-pointer hover:text-mint font-medium" @click="store.setContent('about')">about</a>
-      <a class="text-lg text-lime cursor-pointer font-medium" @click="store.setContent('resume')">resume</a>
+      <span class="text-lg text-lime cursor-pointer hover:text-mint font-medium" @click="store.setContent('home')">home</span>
+      <span class="text-lg text-lime cursor-pointer hover:text-mint font-medium" @click="store.setContent('about')">about</span>
+      <span class="text-lg text-lime cursor-pointer font-medium" @click="store.setContent('resume')">resume</span>
     </nav>
   </header>
 </template>
