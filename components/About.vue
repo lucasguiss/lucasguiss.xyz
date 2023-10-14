22e7150bc38de3cd01e1b1e1470342059bb52704
Lucas Guiss <lucasguissgusmao@gmail.com>
2023-10-14 18:01:54 -0300

    fix: media query for larger screens

--- components/About.vue
+++ components/About.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <div class="flex flex-col justify-start">
+        <div class="flex flex-col justify-start lg:w-128 xl:w-128 2xl:w-128">
             <Hello />
             <p class="text-xl text-grey break-words font-regular">I design and build scalabled build scalabled build
                 scalabled build scalabled build scalable and resilient applications for the web.ns for the web.ns for the
