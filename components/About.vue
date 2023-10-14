cca9a0fadfa7685c55d8b069d92c9305954f5bff
Lucas Guiss <lucasguissgusmao@gmail.com>
2023-10-14 18:11:47 -0300

    feat: add about content text

--- components/About.vue
+++ components/About.vue
@@ -2,13 +2,12 @@
     <section>
         <div class="flex flex-col justify-start lg:w-128 xl:w-128 2xl:w-128">
             <Hello />
-            <p class="text-xl text-grey break-words font-regular">I design and build scalabled build scalabled build
-                scalabled build scalabled build scalable and resilient applications for the web.ns for the web.ns for the
-                web.ns for the web.ns for the web.ns for the webns for the webns for the webns for the webns for the webns
-                for
-                the webns for the webns for the webns for the webns for the webns for the web.
+            <p class="text-xl text-grey break-words font-regular">I've always loved computers and video games for as long as
+                I can remember. <br>I've gone through various stages in the programming world, from working as a backend
+                developer, also dealing with infrastructure and DevOps, to mastering <strong class="text-lime">fullstack development.</strong><br> Outside the
+                world of technology, I have a beloved dog named Gaia, who I love to play outside with, when I'm not gaming, you can find me doing that, running or
+                cycling, hobbies I've been increasingly passionate about.
             </p>
         </div>
 
-    </section>
-</template>
+</section></template>
