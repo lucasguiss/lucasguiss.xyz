d9db651c3eb6c2ce9c2b2ef91e0edfc191a2f5c6
Lucas Guiss <lucasguissgusmao@gmail.com>
2023-10-14 17:56:37 -0300

    feat: add about styling and content

--- components/About.vue
+++ components/About.vue
@@ -1,7 +1,14 @@
 <template>
-    <p class="text-base text-offwhite font-medium">about</p>
+    <section>
+        <div class="flex flex-col justify-start">
+            <Hello />
+            <p class="text-xl text-grey break-words font-regular">I design and build scalabled build scalabled build
+                scalabled build scalabled build scalable and resilient applications for the web.ns for the web.ns for the
+                web.ns for the web.ns for the web.ns for the webns for the webns for the webns for the webns for the webns
+                for
+                the webns for the webns for the webns for the webns for the webns for the web.
+            </p>
+        </div>
+
+    </section>
 </template>
-
-<script setup lang='ts'>
-
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <main class="w-full flex items-center justify-center p-4 sm:p-8 md:p-12">
-    <component :is="componentMapper[store.getContent]" />
+  <main>
+    <div class="w-full flex items-center justify-center p-4 sm:p-8 md:p-12">
+      <component :is="componentMapper[store.getContent]" />
+    </div>
   </main>
 </template>
 <script setup lang="ts">
@@ -8,13 +10,11 @@ import { useMainContentStore } from '@/stores';
 
 const store = useMainContentStore()
 const About = defineAsyncComponent(() => import("@/components/About.vue"))
-const Resume = defineAsyncComponent(() => import("@/components/Resume.vue"))
 const Home = defineAsyncComponent(() => import("@/components/Home.vue"))
 
 const componentMapper = {
   'home': Home,
   'about': About,
-  'resume': Resume,
 }
 
 onBeforeUnmount(() => {
